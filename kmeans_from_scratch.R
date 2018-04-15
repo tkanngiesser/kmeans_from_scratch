b1379e8833adc8e19d2f9a46242724a4c206781e
Tino Kanngießer <tinokanngiesser@Tinos-MBP.fritz.box>
2018-04-15 09:03:47 +0200

    new comments

--- kmeans_from_scratch.R
+++ kmeans_from_scratch.R
@@ -52,6 +52,7 @@ kmeans=function(data,K=4,stop_crit=10e-5)
   return(list(data=data.frame(data,cluster),centroids=centroids))
 }
 
+#test 
 res=kmeans(DF[1:2],K=5)
 res$centroids$cluster=1:5
 res$data$isCentroid=F
